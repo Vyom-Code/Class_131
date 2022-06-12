3c47347831f41a125da794b71c55a28e6ae62c77
Vyom-Code <82333325+Vyom-Code@users.noreply.github.com>
2022-06-12 11:26:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 img = "";
+status ="";
 function setup(){
     canvas = createCanvas(1000, 800);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', loaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 
 function preload(){
@@ -16,4 +19,23 @@ function draw(){
     noFill();
     stroke('#f54b42');
     rect(30, 60, 450, 350);
+
+    fill('#F54b42');
+    text("Cat", 520, 160);
+    noFill();
+    rect(500, 150, 270, 320);
+}
+
+function loaded(){
+console.log("Model Loaded");
+status = true;
+objectDetector.detect(img, gotResult);
+}
+
+function gotResult(){
+    if(error){
+        console.log(error);
+    
+    }
+    console.log(results);
 }
